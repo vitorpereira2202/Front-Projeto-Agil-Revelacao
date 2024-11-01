94dcd7b3296793431ed2ace9fed4a27ae1b1f027
Thaisrs165 <159031641+Thaisrs165@users.noreply.github.com>
2024-11-01 10:40:29 -0300

    def login feito

--- front.py
+++ front.py
@@ -11,7 +11,6 @@ def cadastro():
 
     email = st.text_input("Email", key="signup_email")
     password = st.text_input("Senha", type="password", key="signup_password")
-    
 
     if st.button("Cadastrar"):
         if email in users:
@@ -22,12 +21,30 @@ def cadastro():
             st.session_state["signup_success"] = True
 
 
+def Login():
+    st.title("Login")
+
+    Email = st.text_input("Email", key="login_email")
+    Password = st.text_input("Senha", type="password", key="login_password")
+
+    if st.button("Entrar"):
+        if Email not in users:
+            st.error("Usuário não cadastrado.")
+        elif users[Email] != Password:
+            st.error("Senha incorreta.")
+        else:
+            st.success("Login realizado com sucesso!")
+            st.session_state["logged_in"] = True 
+
+
 def main():
     st.sidebar.title("Menu")
-    option = st.sidebar.radio("Navegar", ["Cadastro"])
+    option = st.sidebar.radio("Navegar", ["Cadastro", "Login"])
 
     if option == "Cadastro":
         cadastro()
+    elif option == "Login":
+        Login()
     
 
 if __name__ == "__main__":
